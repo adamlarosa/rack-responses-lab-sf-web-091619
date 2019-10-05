42b4cea1b3d741dd74ef3ea49015f5560e6d0866
github username <github email address>
2019-10-04 17:35:00 -0700

    Done.

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application
+    def call(env)
+        resp = Rack::Response.new
+        
+        # PULL THE HOUR FROM CURRENT TIME
+        time = Time.now.hour
+        
+        # APPLY LOGIC TO HOUR
+        time < 12 ? (resp.write "Good Morning!") : (resp.write "Good Afternoon!")
+
+        resp.finish
+    end
+end
